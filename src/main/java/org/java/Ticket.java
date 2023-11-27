b32e4910b86be6edd7b3a55b53ba307c8579170e
aalexebr <alex.ebr@icloud.com>
2023-11-27 15:04:11 +0100

    bonus

--- src/main/java/org/java/Ticket.java
+++ src/main/java/org/java/Ticket.java
@@ -2,4 +2,53 @@ package org.java;
 
 public class Ticket {
 
+	private double km;
+	private int age;
+	final static private double pricePerKm = 0.21;
+	final static private double overDiscount = 0.6;
+	final static private double underDiscount = 0.8;
+	
+	public Ticket(double km, int age) throws Exception{
+		setKm(km);
+		setAge(age);
+	}
+
+	public double getKm() {
+		return km;
+	}
+
+	public void setKm(double km) throws Exception{
+		if(km <= 0) {
+			throw new Exception("km must be more than 0");
+		}
+		this.km = km;
+	}
+
+	public int getAge() {
+		return age;
+	}
+
+	public void setAge(int age) throws Exception{
+		if(age <= 0) {
+			throw new Exception("age must be more than 0");
+		}
+		
+		this.age = age;
+	}
+	
+	
+	public double getFullPrice() {
+		if(getAge()>=65) {
+			double price = (pricePerKm * getKm()) * overDiscount;
+			return price;
+		}
+		else if(getAge()< 18) {
+			double price = (pricePerKm * getKm()) * underDiscount;
+			return price;
+		}
+		else {
+			double price = pricePerKm * getKm();
+			return price;
+		}
+	}
 }
--- src/test/java/org/java/TestTicket.java
+++ src/test/java/org/java/TestTicket.java
@@ -1,5 +1,68 @@
 package org.java;
 
-public class TestTicket {
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertThrows;
 
+import org.junit.jupiter.api.BeforeAll;
+import org.junit.jupiter.api.Test;
+
+public class TestTicket {
+	
+	public static Ticket c = null;
+	
+	@Test
+	public void ageTest() {
+		int age = -1;
+		double km = 1;
+		
+		assertThrows(Exception.class,
+				() -> c = new Ticket(km,age), 
+				"Age must be above 0");
+	}
+	
+	@Test
+	public void kmTest() {
+		int age = 1;
+		double km = 0;
+		
+		assertThrows(Exception.class,
+				() -> c = new Ticket(km,age), 
+				"KM must be above 0");
+	}
+	
+	@Test
+	public void noDiscountTest() throws Exception {
+		double pricePerKm = 0.21;
+		double km = 1.1;
+		int age = 18;
+//		double price = km * pricePerKm;
+		double price = 0.231;
+		c = new Ticket(km,age);
+		
+		assertEquals(price, c.getFullPrice(), "PRICE EXPECTED CORRECTLY");
+	}
+	
+	@Test
+	public void discountOver() throws Exception{
+		double pricePerKm = 0.21;
+		double km = 1.1;
+		int age = 65;
+//		double price = km * pricePerKm;
+		double price = 0.1386;
+		c = new Ticket(km,age);
+		
+		assertEquals(price, c.getFullPrice(), "PRICE EXPECTED CORRECTLY");
+	}
+	
+	@Test
+	public void discountUnder() throws Exception{
+		double pricePerKm = 0.21;
+		double km = 101;
+		int age = 17;
+//		double price = km * pricePerKm;
+		double price = 16.968;
+		c = new Ticket(km,age);
+		
+		assertEquals(price, c.getFullPrice(), "PRICE EXPECTED CORRECTLY");
+	}
 }
